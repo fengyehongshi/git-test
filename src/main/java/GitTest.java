652e908d1dbe63933cf2c61c857c68a9af37074c
枫叶红时 <111273743+fengyehongshi@users.noreply.github.com>
2022-10-13 11:01:08 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git qiuqiule");
         System.out.println("push test");
-
+        System.out.println("github fixed code");
     }
 }
